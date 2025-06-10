7ebaee8ce060ee3fe96306653a3056b29e89a8e7
ROHMANSYAH <40875901+rohmansyah123@users.noreply.github.com>
2025-06-10 18:09:40 +0700

    Update navigation.js

--- js/components/navigation.js
+++ js/components/navigation.js
@@ -1,1 +1,18 @@
+// js/components/navigation.js
+
+export const setupNavigation = () => {
+    const navbar = document.querySelector('.navbar');
+    if (!navbar) return;
+
+    const navContent = `
+        <a href="#" class="logo">MovieKu</a>
+        <ul class="nav-links">
+            <li><a href="#">Beranda</a></li>
+            <li><a href="#">Film Populer</a></li>
+            <li><a href="#">Film Mendatang</a></li>
+            <li><a href="#">Genre</a></li>
+        </ul>
+    `;
+    navbar.innerHTML = navContent;
+};
 
